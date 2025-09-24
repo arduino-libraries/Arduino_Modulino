arduino-libraries/Modulino

commit 650968ed4943f3b6d6d27379523a3f300020d664

added support for deadzone on joystick due to rest position drift (#27)

src/Modulino.h
@@ -190,11 +190,20 @@ public:
   bool update() {
     uint8_t buf[3];
     auto res = read((uint8_t*)buf, 3);
-    auto ret = res && (buf[0] != last_status[0] || buf[1] != last_status[1] || buf[2] != last_status[2]);
-    last_status[0] = buf[0];
-    last_status[1] = buf[1];
+    auto x = buf[0];
+    auto y =  buf[1];
+    map_value(x, y);
+    auto ret = res && (x != last_status[0] || buf[1] != y || buf[2] != last_status[2]);
+    if (!ret) {
+      return false;
+    }
+    last_status[0] = x;
+    last_status[1] = y;
     last_status[2] = buf[2];
     return ret;
+  }
+  void setDeadZone(uint8_t dz_th) {
+    _dz_threshold = dz_th;
   }
   PinStatus isPressed() {
     return last_status[2] ? HIGH : LOW;
@@ -213,7 +222,14 @@ public:
     }
     return 0xFF;
   }
+  void map_value(uint8_t &x, uint8_t &y) {
+    if (x > 128 - _dz_threshold &&  x < 128 + _dz_threshold && y > 128 - _dz_threshold && y < 128 + _dz_threshold) {
+        x = 128;
+        y = 128;
+    }
+  }
 private:
+  uint8_t _dz_threshold = 26;
   uint8_t last_status[3];
 protected:
   uint8_t match[1] ={ 0x58 };  // same as fw main.c

arduino-libraries/Arduino_Modulino

commit 80bd731f9efb05ad130993136b0bcf84289aed10

fixes bug introduced in commit 650968ed4943f3b6d6d27379523a3f300020d664

src/Modulino.h
@@ -108,7 +108,7 @@ class Module : public Printable {
 public:
   Module(uint8_t address = 0xFF, const char* name = "", ModulinoHubPort* hubPort = nullptr)
     : address(address), name((char *)name), hubPort(hubPort) {}
-  virtual ~Module() {}  
+  virtual ~Module() {}
   bool begin() {
     if (address >= 0x7F) {
       address = discover() / 2;  // divide by 2 to match address in fw main.c
@@ -267,7 +267,7 @@ public:
     auto x = buf[0];
     auto y =  buf[1];
     map_value(x, y);
-    auto ret = res && (x != last_status[0] || buf[1] != y || buf[2] != last_status[2]);
+    auto ret = res && (x != last_status[0] || y != last_status[1] || buf[2] != last_status[2]);
     if (!ret) {
       return false;
     }
@@ -841,7 +841,7 @@ public:
     if (api == nullptr) {
       return false;
     }
-    
+
     uint8_t NewDataReady = 0;
     api->checkForDataReady(&NewDataReady);
     if (NewDataReady) {