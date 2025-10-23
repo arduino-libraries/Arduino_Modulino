arduino-libraries/Modulino

added support for opto relay

examples/Modulino_Relay/Basic/Basic.ino
@@ -1,0 +1,25 @@
+#include <Modulino.h>
+
+ModulinoRelay relay;
+
+void setup() {
+  Serial.begin(115200);
+  Modulino.begin();
+  relay.begin();
+
+}
+void loop() {
+  relay.ON();
+  if(relay.getStatus())
+  {
+    Serial.println("Relay state ON!");
+  }
+  delay(1000);
+  relay.OFF();
+  if(!(relay.getStatus()))
+  {
+    Serial.println("Relay state OFF!");
+  }
+  Serial.println();
+  delay(1000);
+}

arduino-libraries/Arduino_Modulino

examples/Modulino_Relay/Basic/Basic.ino
@@ -1,0 +1,25 @@
+#include <Modulino.h>
+
+ModulinoRelay relay;
+
+void setup() {
+  Serial.begin(115200);
+  Modulino.begin();
+  relay.begin();
+
+}
+void loop() {
+  relay.on();
+  if(relay.getStatus())
+  {
+    Serial.println("Relay state ON!");
+  }
+  delay(1000);
+  relay.off();
+  if(!(relay.getStatus()))
+  {
+    Serial.println("Relay state OFF!");
+  }
+  Serial.println();
+  delay(1000);
+}