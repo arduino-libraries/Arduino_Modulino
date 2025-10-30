3171b827bc007f5600fa804cc866c6969cff0905
Martino Facchin <m.facchin@arduino.cc>
2025-10-30 17:21:25 +0100

    fix: led_matrix: allow coexistence with UNO R4
    Needs https://github.com/arduino/ArduinoCore-renesas/pull/504 to be merged

--- src/Modulino_LED_Matrix.h
+++ src/Modulino_LED_Matrix.h
@@ -11,16 +11,6 @@
 #define MATRIX_WITH_ARDUINOGRAPHICS
 #endif
 
-static uint32_t reverse(uint32_t x)
-{
-    x = ((x >> 1) & 0x55555555u) | ((x & 0x55555555u) << 1);
-    x = ((x >> 2) & 0x33333333u) | ((x & 0x33333333u) << 2);
-    x = ((x >> 4) & 0x0f0f0f0fu) | ((x & 0x0f0f0f0fu) << 4);
-    x = ((x >> 8) & 0x00ff00ffu) | ((x & 0x00ff00ffu) << 8);
-    x = ((x >> 16) & 0xffffu) | ((x & 0xffffu) << 16);
-    return x;
-}
-
 // TODO: this is dangerous, use with care
 #define loadSequence(frames)                    loadWrapper(frames, sizeof(frames))
 #define renderBitmap(bitmap, rows, columns)     loadPixels(&bitmap[0][0], rows*columns)
@@ -208,6 +198,17 @@ public:
 #endif
 
 private:
+
+    static uint32_t reverse(uint32_t x)
+    {
+        x = ((x >> 1) & 0x55555555u) | ((x & 0x55555555u) << 1);
+        x = ((x >> 2) & 0x33333333u) | ((x & 0x33333333u) << 2);
+        x = ((x >> 4) & 0x0f0f0f0fu) | ((x & 0x0f0f0f0fu) << 4);
+        x = ((x >> 8) & 0x00ff00ffu) | ((x & 0x00ff00ffu) << 8);
+        x = ((x >> 16) & 0xffffu) | ((x & 0xffffu) << 16);
+        return x;
+    }
+
     int _currentFrame = 0;
     uint32_t _frameHolder[3];
     uint32_t* _frames;
