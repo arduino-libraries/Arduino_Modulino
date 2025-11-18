c512ffa234eb17b4eeb418ab600bc2d508893fb3
Gilberto Conti <g.conti@arduino.cc>
2025-11-18 16:33:40 +0100

    example: extend basic vibro with different intensity

--- examples/Modulino_Vibro/Basic/Basic.ino
+++ examples/Modulino_Vibro/Basic/Basic.ino
@@ -8,8 +8,20 @@ void setup() {
 }
 
 void loop() {
-  vibro.on(1000);
+  // Cycle through vibration intensities from GENTLE to MAXIMUM
+  // Each intensity increases by 5, lasting 1 seconds each
+  for (int intensity = GENTLE; intensity <= MAXIMUM; intensity += 5) {
+    Serial.println(intensity);
+    vibro.on(1000, true, intensity);
+    vibro.off();
+    delay(100);
+  }
+
+  // Two vibration of one second separated by a long pause
+  // using the blocking on call
   delay(1000);
-  vibro.off();
+  vibro.on(1000);
+  delay(5000);
+  vibro.on(1000);
   delay(1000);
 }
